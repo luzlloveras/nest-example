587a8d6a691bbf6c8393fa2ffd89c84f5f7fd675
luzlloveras <luzlloveras7@gmail.com>
2023-11-03 18:11:40 -0300

    add

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,5 @@
 /* eslint-disable prettier/prettier */
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -9,6 +9,13 @@ export class AppController {
   @Get('test')
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('/api/items')
+  getItems(@Query()queries): any {
+    const page = queries.page;
+    const items = [1,2,3,4,5,6,7,8,9,10];
+    return items[page - 1];
   }
   
 }
