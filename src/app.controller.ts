2867fa4af089cf8a1ace4568dde4aaef19ec81bc
luzlloveras <luzlloveras7@gmail.com>
2023-11-05 08:30:26 -0300

    add   @Get('/api/items/:id')

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,6 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable prettier/prettier */
-import { Controller, Get, Query } from '@nestjs/common';
+import { Controller, Get, Query, Param } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -16,6 +17,18 @@ export class AppController {
     const page = queries.page;
     const items = [1,2,3,4,5,6,7,8,9,10];
     return items[page - 1];
+  }
+
+  @Get('/api/items/:id')
+  getParam(@Param() params): any {
+    const items = [
+      { id: 1 , name: "item 1"},
+      { id: 2 , name: "item 2"},
+      { id: 3 , name: "item 3"},
+    ];
+    const searchId = params.id;
+    const item = items.find((item) => item.id == searchId);
+    return item;
   }
   
 }
